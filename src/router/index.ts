4fe5e80d070e3de2a9796a9e53bb950c8ccf0ab7
善中   丘 <qiushanzhong@outlook.com>
2020-05-17 21:51:45 +0800

    侧边菜单和面包屑

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Layout from '../views/Layout/index.vue'
+import Layout from '../views/Layout/Index.vue'
 
 Vue.use(VueRouter);
 
