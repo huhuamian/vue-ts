269a5c09e256fd1a117f9280ac71e2f94804048e
善中   丘 <qiushanzhong@outlook.com>
2020-05-19 21:53:23 +0800

    动态渲染左菜单

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,11 +9,16 @@ Vue.use(VueRouter);
       path: '/',
       name: 'dashboard',
       component: Layout,
+      hidden: true,
       redirect: '/home',
       children: [
         {
           path: '/home',
           name: 'home',
+          meta: {
+            title: '首页',
+            icon: 'fa fa-home',
+          },
           component: () => import('@/views/Home.vue')
         }
       ]
@@ -22,22 +27,39 @@ Vue.use(VueRouter);
       path: '/dataManage',
       name: 'dataManage',
       component: Layout,
+      hidden: true,
+      meta: {
+        title: '数据管理',
+        icon: 'fa fa-database'
+      },
       redirect: '/tableData',
       children: [
         {
           path: '/tableData',
           name: 'tableData',
+          meta: {
+            title: '表格管理',
+            icon: 'fa fa-table'
+          },
           component: () => import('@/views/DataManage/TableData.vue')
-        },
-        {
-          path: '/formData',
-          name: 'formData',
-          component: () => import('@/views/DataManage/FormData.vue')
         },
         {
           path: '/chartsData',
           name: 'chartsData',
+          meta: {
+            title: '图表管理',
+            icon: 'fa fa-bar-chart'
+          },
           component: () => import('@/views/DataManage/ChartsData.vue')
+        },
+        {
+          path: '/formData',
+          name: 'formData',
+          meta: {
+            title: '表单管理',
+            icon: 'fa fa-file-text-o'
+          },
+          component: () => import('@/views/DataManage/FormData.vue')
         }
       ]
     },
@@ -45,11 +67,16 @@ Vue.use(VueRouter);
       path: '/userManage',
       name: 'usermanage',
       component: Layout,
+      hidden: true,
       redirect: '/accountData',
       children: [
         {
           path: '/accountData',
           name: 'accountData',
+          meta: {
+            title: '账户管理',
+            icon: 'fa fa-user-plus'
+          },
           component: () => import('@/views/UserManage/AccountData.vue')
         }
       ]
@@ -58,11 +85,15 @@ Vue.use(VueRouter);
       path: '/user',
       name: 'user',
       component: Layout,
+      hidden: false,
       redirect: '/userInfo',
       children: [
         {
           path: '/useriInfo',
           name: 'userInfo',
+          meta: {
+            title: '个人中心'
+          },
           component: () => import('@/views/UserManage/UserInfo.vue')
         }
       ]
@@ -70,16 +101,28 @@ Vue.use(VueRouter);
     {
       path: '/404',
       name: '404',
+      hidden: false,
+      meta: {
+        title: '404',
+      },
       component: () => import('@/views/404.vue')
     },
     {
       path: '/login',
       name: 'Login',
+      hidden: false,
+      meta: {
+        title: '系统登录',
+      },
       component: () => import('@/views/Login/Login.vue')
     },
     {
       path: '/password',
       name: 'Password',
+      hidden: false,
+      meta: {
+        title: '找回密码',
+      },
       component: () => import('@/views/Login/Password.vue')
     },
     {
--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,8 +1,12 @@
 import { MutationTree } from "vuex";
+import router from '@/router';
 
 const mutations: MutationTree<any> = {
     SET_USER(state: any, user: any): void{
         state.user = user;
+    },
+    SET_ROUTERS(state: any, routers: any){
+        state.routers = routers;
     }
 }
 
